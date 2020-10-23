b3db947347a41841c4dd864860214bef5cd1dfe7
Farrukh <farrukhahmad62@gmail.com>
2020-10-23 15:59:50 +0500

    Simple Node Module

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,7 +1,4 @@
-var rect = {
-    perimeter: (x, y) => (2 * (x * y)),
-    area: (x, y) => (x * y)
-};
+var rect = require("./rectangle"); // importing file based node_module
 
 function solveRect(l, b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
